b51215fa688690c8881e9bc310cbcc7a99181a98
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2023-11-22 22:35:53 -0300

    revision de prop styles ButtonMultifuncion

--- src/components/ButtonMultifuncion/ButtonMultifuncion.jsx
+++ src/components/ButtonMultifuncion/ButtonMultifuncion.jsx
@@ -1,15 +1,15 @@
 import estilos from "./ButtonMultifuncion.module.css"
 
 const ButtonMultifuncion = ({color, label, callback}) =>{
-    // let styles = {
-    //     color: {color},  // ¿Por qué la prop no se aplica dentro de styles? -- CONSULTA CHILDREN ACTIVIDAD 2
-    //     border: "solid",
-    //     borderColor: "blue"
-    // }
+    let styles = {
+        color: {color},  // ¿Por qué la prop no se aplica dentro de styles? -- CONSULTA CHILDREN ACTIVIDAD 2
+        border: "solid",
+        borderColor: "blue"
+    }
 
     return(
         <>
-             <button className={estilos.boton} style={{backgroundColor: color}} onClick={callback}> {label} </button>     {/* style={styles} */}
+             <button style={styles} className={estilos.boton}/*  style={{backgroundColor: color}} */ onClick={callback}> {label} </button>     
         </>
     )
 }
